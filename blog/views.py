bd5fb584c964332bbe874e264f5fd2a24a2eadba
artemttt <artemttt@bk.ru>
2018-02-06 21:36:36 +0300

    кнопка удаления

--- blog/views.py
+++ blog/views.py
@@ -27,7 +27,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-    
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -41,3 +41,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('post_list')
